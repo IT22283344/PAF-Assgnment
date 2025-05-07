31dd4efaf421e4163b216415549894de18660bda
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-05-07 10:13:17 +0530

    update the step entity and Todo entity

--- backend/src/main/java/com/skillplus/backend/modal/Step.java
+++ backend/src/main/java/com/skillplus/backend/modal/Step.java
@@ -1,2 +1,44 @@
-package com.skillplus.backend.modal;public class Step {
+package com.skillplus.backend.modal;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+public class Step {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "todo_id")
+    @JsonBackReference
+    private Todo todo;
+
+
+    // Getters and setters
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public Todo getTodo() { return todo; }
+
+    public void setTodo(Todo todo) { this.todo = todo; }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
 }
