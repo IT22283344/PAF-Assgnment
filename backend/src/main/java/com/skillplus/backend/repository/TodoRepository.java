aef3d605a0d2151d70a57e811c7b46d09b661da3
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-05-07 10:54:30 +0530

    add step private final in todo repository

--- backend/src/main/java/com/skillplus/backend/repository/TodoRepository.java
+++ backend/src/main/java/com/skillplus/backend/repository/TodoRepository.java
@@ -1,15 +1,11 @@
-
 package com.skillplus.backend.repository;
 
 import com.skillplus.backend.modal.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-    // Find all todos for a specific user
     List<Todo> findByUserId(Long userId);
+    List<Todo> findByPrivateTodoFalse();
+
 }
