8e75c98b03cd57bf88f5a0d57365ec6aa052b217
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-05-07 10:18:15 +0530

    change step repository interface

--- backend/src/main/java/com/skillplus/backend/repository/StepRepository.java
+++ backend/src/main/java/com/skillplus/backend/repository/StepRepository.java
@@ -1,2 +1,7 @@
-package com.skillplus.backend.repository;public interface StepRepository {
+package com.skillplus.backend.repository;
+
+import com.skillplus.backend.modal.Step;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StepRepository extends JpaRepository<Step, Long> {
 }
